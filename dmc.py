961f4ff86fa0b0a370a0a70bfd8e762ddbc6405d
lgrossman <grossmanlev@gmail.com>
2023-09-23 12:50:44 -0400

    adding compression integration

--- dmc.py
+++ dmc.py
@@ -11,6 +11,8 @@ from dm_control import manipulation, suite
 from dm_control.suite.wrappers import action_scale, pixels
 from dm_env import StepType, specs
 
+from custom_envs import *
+
 
 class ExtendedTimeStep(NamedTuple):
     step_type: Any
@@ -66,11 +68,14 @@ class ActionRepeatWrapper(dm_env.Environment):
 
 
 class FrameStackWrapper(dm_env.Environment):
-    def __init__(self, env, num_frames, pixels_key='pixels'):
+    def __init__(self, env, num_frames, pixels_key="pixels", grayscale=False):
         self._env = env
         self._num_frames = num_frames
         self._frames = deque([], maxlen=num_frames)
         self._pixels_key = pixels_key
+        self._grayscale = grayscale
+        if self._grayscale:
+            print("Using grayscale pixel observations!")
 
         wrapped_obs_spec = env.observation_spec()
         assert pixels_key in wrapped_obs_spec
@@ -79,12 +84,32 @@ class FrameStackWrapper(dm_env.Environment):
         # remove batch dim
         if len(pixels_shape) == 4:
             pixels_shape = pixels_shape[1:]
-        self._obs_spec = specs.BoundedArray(shape=np.concatenate(
-            [[pixels_shape[2] * num_frames], pixels_shape[:2]], axis=0),
-                                            dtype=np.uint8,
-                                            minimum=0,
-                                            maximum=255,
-                                            name='observation')
+
+        if grayscale:
+            self._obs_spec = specs.BoundedArray(
+                shape=np.concatenate([[1 * num_frames], pixels_shape[:2]], axis=0),
+                dtype=np.uint8,
+                minimum=0,
+                maximum=255,
+                name="observation",
+            )
+        else:
+            self._obs_spec = specs.BoundedArray(
+                shape=np.concatenate(
+                    [[pixels_shape[2] * num_frames], pixels_shape[:2]], axis=0
+                ),
+                dtype=np.uint8,
+                minimum=0,
+                maximum=255,
+                name="observation",
+            )
+
+    def _to_grayscale(self, arr, preserve_shape=False):
+        grayscale = np.dot(arr[..., :3], [0.2989, 0.5870, 0.1140]).astype(arr.dtype)
+        grayscale = grayscale[..., np.newaxis]
+        if preserve_shape:
+            return np.repeat(grayscale, 3, axis=-1)
+        return grayscale
 
     def _transform_observation(self, time_step):
         assert len(self._frames) == self._num_frames
@@ -96,6 +121,9 @@ class FrameStackWrapper(dm_env.Environment):
         # remove batch dim
         if len(pixels.shape) == 4:
             pixels = pixels[0]
+        # convert to grayscale
+        if self._grayscale:
+            pixels = self._to_grayscale(pixels)
         return pixels.transpose(2, 0, 1).copy()
 
     def reset(self):
@@ -125,11 +153,13 @@ class ActionDTypeWrapper(dm_env.Environment):
     def __init__(self, env, dtype):
         self._env = env
         wrapped_action_spec = env.action_spec()
-        self._action_spec = specs.BoundedArray(wrapped_action_spec.shape,
-                                               dtype,
-                                               wrapped_action_spec.minimum,
-                                               wrapped_action_spec.maximum,
-                                               'action')
+        self._action_spec = specs.BoundedArray(
+            wrapped_action_spec.shape,
+            dtype,
+            wrapped_action_spec.minimum,
+            wrapped_action_spec.maximum,
+            "action",
+        )
 
     def step(self, action):
         action = action.astype(self._env.action_spec().dtype)
@@ -164,11 +194,13 @@ class ExtendedTimeStepWrapper(dm_env.Environment):
         if action is None:
             action_spec = self.action_spec()
             action = np.zeros(action_spec.shape, dtype=action_spec.dtype)
-        return ExtendedTimeStep(observation=time_step.observation,
-                                step_type=time_step.step_type,
-                                action=action,
-                                reward=time_step.reward or 0.0,
-                                discount=time_step.discount or 1.0)
+        return ExtendedTimeStep(
+            observation=time_step.observation,
+            step_type=time_step.step_type,
+            action=action,
+            reward=time_step.reward or 0.0,
+            discount=time_step.discount or 1.0,
+        )
 
     def observation_spec(self):
         return self._env.observation_spec()
@@ -180,21 +212,32 @@ class ExtendedTimeStepWrapper(dm_env.Environment):
         return getattr(self._env, name)
 
 
-def make(name, frame_stack, action_repeat, seed):
-    domain, task = name.split('_', 1)
+def make(name, frame_stack, action_repeat, seed, black_floor=False, grayscale=False):
+    domain, task = name.split("_", 1)
     # overwrite cup to ball_in_cup
-    domain = dict(cup='ball_in_cup').get(domain, domain)
+    domain = dict(cup="ball_in_cup").get(domain, domain)
     # make sure reward is not visualized
     if (domain, task) in suite.ALL_TASKS:
-        env = suite.load(domain,
-                         task,
-                         task_kwargs={'random': seed},
-                         visualize_reward=False)
-        pixels_key = 'pixels'
+        if black_floor:
+            print("Making floor black!")
+            if domain == "quadruped" and task == "walk":
+                env = make_quadruped_walk(seed)
+            elif domain == "walker" and task == "walk":
+                env = make_walker_walk(seed)
+            elif domain == "humanoid" and task == "walk":
+                env = make_humanoid_walk(seed)
+            else:
+                print("Invalid domain and task for black floor!")
+                return None
+        else:
+            env = suite.load(
+                domain, task, task_kwargs={"random": seed}, visualize_reward=False
+            )
+        pixels_key = "pixels"
     else:
-        name = f'{domain}_{task}_vision'
+        name = f"{domain}_{task}_vision"
         env = manipulation.load(name, seed=seed)
-        pixels_key = 'front_close'
+        pixels_key = "front_close"
     # add wrappers
     env = ActionDTypeWrapper(env, np.float32)
     env = ActionRepeatWrapper(env, action_repeat)
@@ -204,10 +247,8 @@ def make(name, frame_stack, action_repeat, seed):
         # zoom in camera for quadruped
         camera_id = dict(quadruped=2).get(domain, 0)
         render_kwargs = dict(height=84, width=84, camera_id=camera_id)
-        env = pixels.Wrapper(env,
-                             pixels_only=True,
-                             render_kwargs=render_kwargs)
+        env = pixels.Wrapper(env, pixels_only=True, render_kwargs=render_kwargs)
     # stack several frames
-    env = FrameStackWrapper(env, frame_stack, pixels_key)
+    env = FrameStackWrapper(env, frame_stack, pixels_key, grayscale=grayscale)
     env = ExtendedTimeStepWrapper(env)
     return env
